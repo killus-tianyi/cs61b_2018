3b9d89c56152b7b60f068baea34b6d751447d9b6
killus-tianyi <tian35682@gmail.com>
2023-10-06 21:23:58 +0800

    fix it in 2th

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -35,14 +35,7 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year)
     {
-	    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-	    {
-		    return true;
-	    }
-	    else
-	    {
-		    return false;
-	    }
+	    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
     }
 
 }
